29d3a0f02546e8f1bdeb859f184f45cb125c3edb
Adrian Haubrich <adrianluca13@gmail.com>
2025-02-19 16:39:51 +0100

    Initial Commit

---
+++ WhisperUITests/WhisperUITests.swift
@@ -0,0 +1,16 @@
+//
+//  WhisperUITests.swift
+//  WhisperUITests
+//
+//  Created by Adrian Haubrich on 19.02.25.
+//
+
+import Testing
+
+struct WhisperUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
